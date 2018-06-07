899b5ee7ff5fcff6eaba5996c8182aefaa65573d
Howard Ding <hding2@illinois.edu>
2018-06-07 11:20:17 -0500

    Adapt to use medusa_storage gem Adapt to compute fixities from an IO instead of directly from file

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source 'https://rubygems.org'
 
 gem 'simple-amqp-server', git: 'https://github.com/medusa-project/simple-amqp-server-jruby.git'
+gem 'medusa_storage', git: 'https://github.com/medusa-project/medusa_storage.git'
--- lib/medusa_fixity_server.rb
+++ lib/medusa_fixity_server.rb
@@ -3,26 +3,28 @@ require 'digest'
 
 class MedusaFixityServer < SimpleAmqpServer::Base
 
-  attr_accessor :content_root
+  attr_accessor :content_root, :storage_roots, :default_root
 
   FIXITY_ALGORITHM_HASH = {'md5' => Digest::MD5, 'sha1' => Digest::SHA1}
 
   def initialize(args = {})
     super(args)
     self.content_root = self.config.content(:root)
+    self.default_root = self.config.medusa_storage(:default_root)
+    self.storage_roots = MedusaStorage::RootSet.new(self.config.medusa_storage(:roots))
   end
 
   def handle_file_fixity_request(interaction)
-    relative_path = interaction.request_parameter('path')
-    self.logger.info "Computing fixity for: #{relative_path}"
-    absolute_path = File.join(self.content_root, relative_path)
-    unless File.exists?(absolute_path)
+    key = interaction.request_parameter('path')
+    storage_root = self.storage_roots.at(request_parameter('root') || self.default_root)
+    self.logger.info "Computing fixity for: #{key} for root #{storage_root}"
+    unless storage_root.exist?(key)
       interaction.succeed(checksums: {}, found: false)
       return
     end
     fixities = Hash.new
     find_algorithms(interaction.request_parameter('algorithms')).each do |algorithm|
-      fixities[algorithm] = compute_fixity(absolute_path, algorithm)
+      fixities[algorithm] = compute_fixity(storage_root, key, algorithm)
     end
     self.logger.info "Computed fixities: #{fixities}"
     interaction.succeed(checksums: fixities, found: true)
@@ -35,8 +37,15 @@ class MedusaFixityServer < SimpleAmqpServer::Base
     algorithms
   end
 
-  def compute_fixity(path, algorithm)
-    FIXITY_ALGORITHM_HASH[algorithm].send(:file, path).to_s
+  def compute_fixity(storage_root, key, algorithm)
+    hasher = FIXITY_ALGORITHM_HASH[algorithm].new
+    buffer = ''
+    storage_root.with_input_io(key) do |io|
+      while io.read(65536, buffer)
+        hasher << buffer
+      end
+    end
+    hasher.to_s
   end
 
 end
